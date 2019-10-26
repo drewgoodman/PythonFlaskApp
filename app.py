16f13be87c74bf041eb26a23bde0b105503718a6
rdogm13 <drg6525@yahoo.com>
2019-10-26 12:31:22 -0700

    new test

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def login():
             if sha256_crypt.verify(password_candidate, password):
                 session['logged_in'] = True
                 session['username'] = username
-                flash("You are now logged in",'success')
+                # flash("You are now logged in",'success')
                 return redirect(url_for('dashboard'))
                 
             else:
